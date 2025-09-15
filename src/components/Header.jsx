8695c51710dd0db33cb6e5972f13f7b7e4a29b42
Prithvi Rajput <prithvirajput986@gmail.com>
2025-09-16 03:19:33 +0530

    finally completed the project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -96,17 +96,17 @@ export default function Header() {
         {/* Desktop CTA */}
         <div className="hidden md:flex items-center gap-4">
           <Link
-            to="/appointment"
+            to={`/book`}
             className="px-4 py-2 rounded-md bg-primary text-white font-medium shadow-sm hover:scale-[1.02] transition-transform"
           >
             Book
           </Link>
-          <a
+          {/* <a
             href="tel:+911234567890"
             className="flex items-center gap-2 text-lighttext"
           >
             <FiPhone /> +91 12345 67890
-          </a>
+          </a> */}
           <LanguageDropdown />
         </div>
 
@@ -118,11 +118,11 @@ export default function Header() {
         >
           {open ? <FiX size={22} /> : <FiMenu size={22} />}
         </button>
-        <button
+        {/* <button
 
           onClick={() => console.log(language)}>
           click
-        </button>
+        </button> */}
       </div>
 
       {/* Mobile Menu */}
